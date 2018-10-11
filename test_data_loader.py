e9bee7232ac86b0daeb976d6c98952360c39c7c0
Con-MI <aspris.marios@gmail.com>
2018-10-11 11:20:26 +0200

    bg

--- test_data_loader.py
+++ test_data_loader.py
@@ -2,9 +2,12 @@ from data_loader import Melanoma_Train_Validation_DataLoader
 from torchvision import transforms
 from fcn_naive_model import fcn_model
 
+use_cuda = torch.cuda.is_available()
 segm_model = fcn_model()
 train_loader, validation_loader = Melanoma_Train_Validation_DataLoader(batch_size = 4, data_transforms=transforms.Compose([transforms.Resize([512, 512]), transforms.ToTensor()]))
-
+if use_cuda
+    segm_model.cuda()
+segm_model.train()
 for i, sample in enumerate(validation_loader):
     img, label_img = sample
     output = segm_model(img)
