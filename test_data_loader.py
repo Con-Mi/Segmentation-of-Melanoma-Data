a41b4c4bb58b3b206cda28778128bf32e7fb8718
Con-MI <aspris.marios@gmail.com>
2018-10-11 11:17:54 +0200

    bg

--- test_data_loader.py
+++ test_data_loader.py
@@ -1,10 +1,14 @@
 from data_loader import Melanoma_Train_Validation_DataLoader
 from torchvision import transforms
+from fcn_naive_model import fcn_model
 
-train_loader, validation_loader = Melanoma_Train_Validation_DataLoader(batch_size = 1, data_transforms=transforms.Compose([transforms.Resize([512, 512]), transforms.ToTensor()]))
+segm_model = fcn_model()
+train_loader, validation_loader = Melanoma_Train_Validation_DataLoader(batch_size = 4, data_transforms=transforms.Compose([transforms.Resize([512, 512]), transforms.ToTensor()]))
 
 for i, sample in enumerate(validation_loader):
     img, label_img = sample
-    print(img.size())
+    output = segm_model(img)
+    print(output.size())
     print(label_img.size())
     print(type(img))
+
