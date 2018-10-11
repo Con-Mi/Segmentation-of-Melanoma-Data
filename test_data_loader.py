58ad5a496d776d0f6abe64a6f3106e42f28b89a9
Con-MI <aspris.marios@gmail.com>
2018-10-11 11:20:55 +0200

    bg

--- test_data_loader.py
+++ test_data_loader.py
@@ -5,7 +5,7 @@ from fcn_naive_model import fcn_model
 use_cuda = torch.cuda.is_available()
 segm_model = fcn_model()
 train_loader, validation_loader = Melanoma_Train_Validation_DataLoader(batch_size = 4, data_transforms=transforms.Compose([transforms.Resize([512, 512]), transforms.ToTensor()]))
-if use_cuda
+if use_cuda:
     segm_model.cuda()
 segm_model.train()
 for i, sample in enumerate(validation_loader):
