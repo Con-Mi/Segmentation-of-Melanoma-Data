c7c0b155f3808740a2e75bece2e94801a222b500
Con-MI <aspris.marios@gmail.com>
2018-10-16 13:48:40 +0200

    Updates

---
+++ mul_preds_linknet.py
@@ -0,0 +1,56 @@
+import torch
+from PIL import Image
+from torchvision import transforms
+from matplotlib import pyplot as plt
+from torchvision import utils
+
+from linknet import linknet_model
+from helper import load_model, make_mask_overlay, get_ids_from_file_in_list
+
+import time
+import cv2
+import numpy as np
+
+thrs = 0.45400802
+upper = 1
+lower = 0
+
+start_time = time.time()
+segm_model = linknet_model()
+segm_model = load_model(segm_model, model_dir="linknet_10epch.pt")
+direc = "./TestImages/"
+saved_dir = "./TestResultsImagesLink/"
+list_ids = get_ids_from_file_in_list(direc)
+
+for img_id in list_ids:
+    img = Image.open(direc + img_id)
+    trf = transforms.Compose([transforms.Resize([512, 512]), transforms.ToTensor()])
+    img_input = trf(img)
+    img_input = img_input.unsqueeze(dim=0)
+    output = segm_model(img_input)
+    output = torch.sigmoid(output)
+
+    inference_time = time.time() - start_time
+    print("Time to do inference was: {:.0f} seconds and {:.5f} micro seconds ".format(inference_time, inference_time % 60))
+    output = output.squeeze(0)
+    output = output.permute(1, 2, 0)
+    output = output.squeeze()
+    output_np = output.detach().numpy()
+    binary_output = np.where(output_np > thrs, upper, lower)
+    plt.imsave(saved_dir + "bitmap_" + img_id + ".png", binary_output, cmap = "tab20b")
+
+    save_img = img_input.squeeze(0)
+    save_img = save_img.permute(1, 2, 0)
+    save_img = save_img.squeeze()
+    save_img = save_img.detach().numpy()
+    plt.imsave(saved_dir + "original_" + img_id + "_sample.png", save_img)
+
+    orig_img = cv2.imread(saved_dir + "original_" + img_id + "_sample.png")
+    mask = cv2.imread(saved_dir + "bitmap_" + img_id + ".png")
+
+    overlayed_img = cv2.addWeighted(orig_img, 1, mask, 0.3, 0, 0)
+
+    cv2.imwrite(saved_dir + "overlayed_" + img_id + "_mask.png", overlayed_img)
+
+end_program_time = time.time() - start_time
+print("Total time to run inference and save the image: {:.0f} seconds and {:.5f} micro seconds ".format(end_program_time, end_program_time % 60))
---
+++ optimal_thrs_linknet.py
@@ -0,0 +1,39 @@
+import torch
+import numpy as np
+from torchvision import transforms
+import pandas as pd
+
+from linknet import linknet_model
+from data_loader import Melanoma_Train_Validation_DataLoader
+from helper import jaccard, dice, load_model
+
+use_cuda = torch.cuda.is_available()
+
+# Hyperparameters
+thrs_list = np.linspace(0.1, 0.9, 400) 
+batch_size = 4
+num_workers = 10
+
+_, validation_loader = Melanoma_Train_Validation_DataLoader(batch_size = batch_size,  data_transforms=transforms.Compose([transforms.Resize([512, 512]), transforms.ToTensor()]), num_workers=num_workers)
+
+model = linknet_model().cuda() if use_cuda else linknet_model()
+model = load_model(model, model_dir="linknet_10epch.pt", map_location_device="gpu") if use_cuda else load_model(model, model_dir="linknet_10epch.pt")
+columns = ["Threshold", "Accuracy"]
+thrs_df = pd.DataFrame(columns = columns)
+thrs_df["Threshold"] = thrs_list
+
+for thrs in thrs_list:
+    jaccard_acc = 0.0
+    for input_img, label_img in validation_loader:
+        input_img = input_img.cuda() if use_cuda else input_img
+        label_img = label_img.cuda() if use_cuda else label_img
+        outputs = model(input_img)
+        preds = torch.sigmoid(outputs)
+        jaccard_acc += jaccard(label_img, (preds > thrs).float())
+    print("Threshold {:.8f} | Jaccard Accuracy: {:.8f}".format(thrs, jaccard_acc / len(validation_loader)))
+    thrs_df["Accuracy"] = (jaccard_acc / len(validation_loader))
+
+idx = thrs_df.loc[thrs_df["Accuracy"] == max(thrs_df["Accuracy"])]
+optimal_thrs = thrs_df["Threshold"].loc[idx.index.values]
+thrs_df.to_csv("accuracyVSthreshold.csv")
+print("Optimal Threshold is {:.8f} found with Accuracy of {:.4f}".format(optimal_thrs.values, max(thrs_df["Accuracy"])))
